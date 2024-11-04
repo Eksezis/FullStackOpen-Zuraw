c229c7976974ac94837ff311d1342653374c9005
Eksezis <official.eksezis@gmail.com>
2024-11-04 14:33:33 +0100

    Ex1 1.6

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -1,38 +1,35 @@
-const Header = (props)=>{ return(<h1>{props.course}</h1>)}
-const Part = (props)=>{return(<p>{props.part} {props.exercises}</p>)}
-const Content = (props)=>{
-  return(<div>
-    <Part part={props.parts[0].name} exercises={props.parts[0].exercises}/>
-    <Part part={props.parts[1].name} exercises={props.parts[1].exercises}/>
-    <Part part={props.parts[2].name} exercises={props.parts[2].exercises}/>
-  </div>)
-}
-const Total = (props)=>{return(<p>Number of exercises {props.parts[0].exercises + props.parts[1].exercises + props.parts[2].exercises}</p>)}
-
+import { useState } from 'react'
+const Title = (props)=>{ return(<h1>{props.title}</h1>)}
+const Total = (props)=>{ return(<p>{props.name} {props.number}</p>) }
 const App = () => {
-  const course = {
-    name: 'Half Stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7
-      },
-      {
-        name: 'State of a component',
-        exercises: 14
-      }
-    ]
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+  const titles = {
+    first: 'give feedback',
+    second:'statistic'
+  }
+  const handleGood = () => {
+    setGood(good + 1)
+  }
+  const handleNeutral = () => {
+    setNeutral(neutral + 1)
+  }
+  const handleBad = () => {
+    setBad(bad + 1)
   }
 
   return (
     <div>
-      <Header course={course.name} />
-      <Content parts={course.parts} />
-      <Total parts={course.parts} />
+      <Title title={titles.first} />
+      <button onClick={handleGood}>good</button>
+      <button onClick={handleNeutral}>neutral</button>
+      <button onClick={handleBad}>bad</button>
+      <Title title={titles.second} />
+      <Total name="good" number={good}/>
+      <Total name="neutral" number={neutral}/>
+      <Total name="bad" number={bad}/>
     </div>
   )
 }
