5abe9e40b9813566ce5d65fdba6c58b0dc36e2d8
Eksezis <official.eksezis@gmail.com>
2025-01-20 14:33:56 +0100

    Ex2 part2.2

--- part2a/src/App.jsx
+++ part2a/src/App.jsx
@@ -9,11 +9,15 @@ const Header = ({name}) => {return(
   <h1>{name}</h1>
 )}
 
-const Content = ({parts}) => {return(
+const Content = ({parts}) => {
+  const numberArray = parts.map(part => Number(part.exercises))
+  const total = numberArray.reduce((total, array_item) => total + array_item, 0)
+  return(
   <div>
     {parts.map(part => (
       <Part part={part}/>
     ))}
+    <b>total of {total} exercises</b>
   </div>
 )}
 
