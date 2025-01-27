9b2c6ac99b83bb4efeed7a170d6b2c6c3af3851c
Eksezis <official.eksezis@gmail.com>
2025-01-27 13:55:27 +0100

    Ex2 part2.6

---
+++ part2b/src/App.jsx
@@ -0,0 +1,42 @@
+import { useState } from 'react'
+
+const App = () => {
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas' }
+  ]) 
+  const [newName, setNewName] = useState('')
+
+  const SubmitChange = (event) => {
+    event.preventDefault()
+    const newPerson = { name: newName }
+    setPersons(persons.concat(newPerson))
+    setNewName('')
+  }
+
+  const ChangeName = (event) => {
+    setNewName(event.target.value)
+  }
+
+  const personList = persons.map(person => (
+    <p>{person.name}</p>
+  ))
+  
+  return (
+    <div>
+      <div>debug: {newName}</div>
+      <h2>Phonebook</h2>
+      <form onSubmit={SubmitChange}>
+        <div>
+          name: <input value={newName} onChange={ChangeName} />
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+        {personList}
+    </div>
+  )
+}
+
+export default App
