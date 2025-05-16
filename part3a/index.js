027c30b62668d81c6a4ccf025fcd1604003f3583
Eksezis <official.eksezis@gmail.com>
2025-05-16 07:57:23 +0200

    Ex3 Part3a.3

--- part3a/index.js
+++ part3a/index.js
@@ -35,7 +35,16 @@ app.get('/info', (request, response) => {
   )
 })
 app.get('/api/persons', (request, response) => {
-    response.json(notes)
+    response.json(persons)
+})
+app.get('/api/persons/:id', (request, response) => {
+    const id = request.params.id
+    const person = persons.find(person => person.id === id)
+    if (person) {
+        response.json(person)
+    } else {
+        response.status(404).json({Error: 'Person does not exist' }).end()
+    }
 })
 
 
