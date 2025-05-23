b9c300063088283c35a20e80884c7a4e7e8deb71
Eksezis <official.eksezis@gmail.com>
2025-05-23 08:09:22 +0200

    Ex3 part3.9

---
+++ part3b/src/App.jsx
@@ -0,0 +1,101 @@
+import { useState, useEffect } from 'react'
+import Search from './components/Search'
+import PersonForm from './components/PersonForm'
+import PersonList from './components/PersonList'
+import ServerLogic from './services/ServerLogic'
+
+const App = () => {
+  const [persons, setPersons] = useState([])
+  const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
+  const [search, setSearch] = useState('')
+  const [Message, setMessage] = useState(null)
+  const [MessageStyle, setMessageStyle] = useState('green')
+
+  useEffect(() => {
+    ServerLogic.getPersonList()
+      .then(data => {setPersons(data);})
+      .catch(error => {
+        setMessageStyle('red')
+        setMessage(`Failed fetching data: `+error)
+        setTimeout(() => { setMessage(null) }, 5000)
+      });
+    },[]
+  )
+
+  const SubmitChange = (event) => {
+    event.preventDefault()
+    if (persons.some(person => person.name === newName)) {
+        setMessageStyle('red')
+        setMessage(`${newName} is already added to phonebook`)
+        setTimeout(() => { setMessage(null) }, 5000)
+      return
+    }
+    if (persons.some(person => person.number === newNumber)) {
+        setMessageStyle('red')
+        setMessage(`${newNumber} is already added to phonebook`)
+        setTimeout(() => { setMessage(null) }, 5000)
+      return
+    }
+    ServerLogic.submitPerson(newName, newNumber)
+      .then(data => {
+        setPersons(persons.concat(data));
+          setMessageStyle('green')
+          setMessage(`${newName} added successfully`)
+          setTimeout(() => { setMessage(null) }, 5000)
+      })
+      .catch(error => {
+          setMessageStyle('red')
+          setMessage(`Failed adding person: `+error)
+          setTimeout(() => { setMessage(null) }, 5000)
+      });
+  }
+  
+
+  const Notification = ({ message, messageStyle }) => {
+    if (message === null) {
+      return null
+    }
+    if (messageStyle == 'green') {
+      return (<div className='notif'>{message}</div>)
+    }
+    if (messageStyle == 'red') {
+      return (<div className='error'>{message}</div>)
+    }
+  }
+
+  const NameChange = (event) => {
+    setNewName(event.target.value)
+  }
+
+  const NumberChange = (event) => {
+    setNewNumber(event.target.value)
+  }
+
+  const SearchChange = (event) => {
+    setSearch(event.target.value)
+  }
+
+  const filteredPersons = persons.filter(person =>
+    person.name.toLowerCase().includes(search.toLowerCase())
+  )
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <Notification message={Message} messageStyle={MessageStyle}/>
+      <Search search={search} onSearchChange={SearchChange} />
+      <h2>Add a new</h2>
+      <PersonForm 
+        newName={newName} 
+        newNumber={newNumber} 
+        onNameChange={NameChange} 
+        onNumberChange={NumberChange} 
+        onSubmit={SubmitChange} 
+      />
+      <PersonList filteredPersons={filteredPersons} />
+    </div>
+  )
+}
+
+export default App
---
+++ part3b/src/components/PersonList.jsx
@@ -0,0 +1,10 @@
+const PersonList = ({ filteredPersons, Delete }) => (
+    <div>
+      <h2>Numbers</h2>
+      {filteredPersons.map(person => (
+        <p key={person.id}>{person.name} {person.number}</p>
+      ))}
+    </div>
+  )
+
+export default PersonList
