67fe481594bb60add1f81d487c2898275aab45e0
Eksezis <official.eksezis@gmail.com>
2025-05-23 08:49:08 +0200

    Ex3 part3.9 poprawa

--- part3b/src/components/Search.jsx
+++
@@ -1,8 +0,0 @@
-const Search = ({ search, onSearchChange }) => (
-    <div>
-      filter shown with: 
-      <input value={search} onChange={onSearchChange} />
-    </div>
-  )
-
-  export default Search
