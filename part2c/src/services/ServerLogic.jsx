d19ed74c638478ad18c183416949a7386c36d4c5
Eksezis <official.eksezis@gmail.com>
2025-05-23 08:59:51 +0200

    Ex3 part3.9 poprawa

--- part2c/src/services/ServerLogic.jsx
+++ part2c/src/services/ServerLogic.jsx
@@ -1,19 +1,10 @@
 import axios from "axios";
+const baseurl = "/api/persons"
   const getPersonList = () => {
-    return axios.get('http://localhost:3001/persons').then(response => response.data);
+    return axios.get(baseurl).then(response => response.data);
   };
   const submitPerson = (newName, newNumber) => {
     const newPerson = { name: newName, number: newNumber };
-    return axios.post('http://localhost:3001/persons', newPerson).then(response => response.data);
+    return axios.post(baseurl, newPerson).then(response => response.data);
   };
-  const Delete = (id) => {
-    return axios.delete(`http://localhost:3001/persons/${id}`)
-      .then(() => {console.log(`Person with ID ${id} deleted successfully.`);})
-      .catch(error => {console.error('Error deleting person:', error);alert('Failed to delete the person');});
-  };
-  const updateNumber = (id, newNumber) => {
-    return axios.patch(`http://localhost:3001/persons/${id}`, {number: newNumber})
-      .then(response => {return response.data;})
-      .catch(error => {console.error('Error updating resource:', error);});
-  };
-export default { getPersonList, submitPerson, Delete, updateNumber };
+export default { getPersonList, submitPerson};
