b1b4bb64883c427ffcc7df250dcfb012e8586766
Eksezis <official.eksezis@gmail.com>
2025-05-29 09:25:20 +0200

    Revert "Ex3 part3.9 poprawa"
    This reverts commit d19ed74c638478ad18c183416949a7386c36d4c5.

--- part2c/src/services/ServerLogic.jsx
+++ part2c/src/services/ServerLogic.jsx
@@ -1,10 +1,19 @@
 import axios from "axios";
-const baseurl = "/api/persons"
   const getPersonList = () => {
-    return axios.get(baseurl).then(response => response.data);
+    return axios.get('http://localhost:3001/persons').then(response => response.data);
   };
   const submitPerson = (newName, newNumber) => {
     const newPerson = { name: newName, number: newNumber };
-    return axios.post(baseurl, newPerson).then(response => response.data);
+    return axios.post('http://localhost:3001/persons', newPerson).then(response => response.data);
   };
-export default { getPersonList, submitPerson};
+  const Delete = (id) => {
+    return axios.delete(`http://localhost:3001/persons/${id}`)
+      .then(() => {console.log(`Person with ID ${id} deleted successfully.`);})
+      .catch(error => {console.error('Error deleting person:', error);alert('Failed to delete the person');});
+  };
+  const updateNumber = (id, newNumber) => {
+    return axios.patch(`http://localhost:3001/persons/${id}`, {number: newNumber})
+      .then(response => {return response.data;})
+      .catch(error => {console.error('Error updating resource:', error);});
+  };
+export default { getPersonList, submitPerson, Delete, updateNumber };
