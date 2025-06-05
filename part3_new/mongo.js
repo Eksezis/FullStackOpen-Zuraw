310ff5e172910eb8e79e0347c71e65c7a664162e
Eksezis <official.eksezis@gmail.com>
2025-06-05 09:13:35 +0200

    Ex3 part3.12

---
+++ part3_new/mongo.js
@@ -0,0 +1,39 @@
+import mongoose from 'mongoose'
+
+const password = process.argv[2]
+const name = process.argv[3]
+const number = process.argv[4]
+
+const url = `mongodb+srv://Eksezis:${password}@cluster0.ak0xhj2.mongodb.net/phonebook?retryWrites=true&w=majority&appName=Cluster0`
+
+mongoose.set('strictQuery',false)
+mongoose.connect(url)
+
+const personSchema = new mongoose.Schema({
+  name: String,
+  number: String,
+  important: Boolean,
+})
+
+const Person = mongoose.model('Person', personSchema)
+
+if(!name && !number){
+    Person.find({ important: true }).then(result => {
+      console.log('Phonebook:')
+      result.forEach(person => {
+        console.log(`${person.name} ${person.number}`)
+      })
+      return mongoose.connection.close()
+    })
+}else{
+    const person = new Person({
+        name: name,
+        number: number,
+        important: true,
+      })
+      
+      person.save().then(result => {
+        console.log(`added ${name} number ${number} to phonebook`)
+        mongoose.connection.close()
+      })
+}
